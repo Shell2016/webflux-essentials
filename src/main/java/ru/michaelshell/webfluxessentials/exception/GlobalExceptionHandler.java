04a42e81ced7278869b56af9d65e5be3c5365189
Shell2016 <bvg2003@bk.ru>
2023-05-27 21:16:08 +0300

    lesson 8

---
+++ src/main/java/ru/michaelshell/webfluxessentials/exception/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package ru.michaelshell.webfluxessentials.exception;
+
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.error.ErrorAttributeOptions.Include;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Order(-2)
+@Component
+public class GlobalExceptionHandler extends AbstractErrorWebExceptionHandler {
+
+    public GlobalExceptionHandler(ErrorAttributes errorAttributes,
+                                  WebProperties.Resources resources,
+                                  ApplicationContext applicationContext,
+                                  ServerCodecConfigurer codecConfigurer) {
+        super(errorAttributes, resources, applicationContext);
+        this.setMessageWriters(codecConfigurer.getWriters());
+    }
+
+    @Override
+    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
+        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse);
+    }
+
+    private Mono<ServerResponse> renderErrorResponse(
+            ServerRequest request) {
+
+        Map<String, Object> errorPropertiesMap = getErrorAttributes(request,
+                ErrorAttributeOptions.of(Include.STACK_TRACE));
+        int status = (int) Optional.ofNullable(errorPropertiesMap.get("status")).orElse(500);
+        return ServerResponse.status(status)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(errorPropertiesMap));
+    }
+}
