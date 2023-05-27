fc6d32c6346fc96b01fde65ec2b690a21f38f9dc
Shell2016 <bvg2003@bk.ru>
2023-05-27 21:49:20 +0300

    lesson 10

---
+++ src/main/java/ru/michaelshell/webfluxessentials/exception/GlobalErrorAttributes.java
@@ -0,0 +1,25 @@
+package ru.michaelshell.webfluxessentials.exception;
+
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+
+@Component
+public class GlobalErrorAttributes extends DefaultErrorAttributes {
+
+    @Override
+    public Map<String, Object> getErrorAttributes(ServerRequest request, ErrorAttributeOptions options) {
+        Map<String, Object> map = super.getErrorAttributes(request, options);
+        Throwable throwable = getError(request);
+        if (throwable instanceof ResponseStatusException) {
+            ResponseStatusException ex = (ResponseStatusException) throwable;
+            map.put("message", ex.getMessage());
+            map.put("developerMessage", "Dev message example");
+        }
+        return map;
+    }
+}
--- src/main/java/ru/michaelshell/webfluxessentials/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/michaelshell/webfluxessentials/exception/GlobalExceptionHandler.java
@@ -3,13 +3,13 @@ package ru.michaelshell.webfluxessentials.exception;
 import org.springframework.boot.autoconfigure.web.WebProperties;
 import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
-import org.springframework.boot.web.error.ErrorAttributeOptions.Include;
 import org.springframework.boot.web.reactive.error.ErrorAttributes;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.*;
 import reactor.core.publisher.Mono;
@@ -36,12 +36,20 @@ public class GlobalExceptionHandler extends AbstractErrorWebExceptionHandler {
 
     private Mono<ServerResponse> renderErrorResponse(
             ServerRequest request) {
+        String query = request.uri().getQuery();
 
+        ErrorAttributeOptions errorAttributeOptions = isTraceEnabled(query)
+                ? ErrorAttributeOptions.of(ErrorAttributeOptions.Include.STACK_TRACE)
+                : ErrorAttributeOptions.defaults();
         Map<String, Object> errorPropertiesMap = getErrorAttributes(request,
-                ErrorAttributeOptions.of(Include.STACK_TRACE));
+                errorAttributeOptions);
         int status = (int) Optional.ofNullable(errorPropertiesMap.get("status")).orElse(500);
         return ServerResponse.status(status)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromValue(errorPropertiesMap));
+    }
+
+    private boolean isTraceEnabled(String query) {
+        return StringUtils.hasText(query) && query.contains("trace=true");
     }
 }
